3d1ce51a3a198e877751edfecbf6e20c743c09cc
David Rambo <davidrambo@mailfence.com>
2023-05-09 16:35:59 -0700

    Add bloom and height to flower model

--- src/flower_store/models.py
+++ src/flower_store/models.py
@@ -13,6 +13,26 @@ class Flower(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     stock = db.Column(db.Integer, default=0)
     image_file = db.Column(db.String(20))
+    bloom_size = db.Column(db.Float(5))  # in inches
+    height = db.Column(db.Float(5))  # in feet
+    # form = db.Column(db.String(40))
+    # color = db.Column(db.String(20))
 
     def __repr__(self):
         return f"<Flower: {self.name}, {self.stock}>"
+
+
+def dev_populate():
+    """Populates the database with Flowers for the sake of development."""
+    from random import randint
+
+    flowers = [
+        "KA's Bella Luna",
+        "KA's Blood Orange",
+        "KA's Boho Peach",
+        "KA's Mocha Jake",
+        "KA's Mocha Maya",
+    ]
+
+    for flower in flowers:
+        db.session.add(Flower(name=flower, stock=randint(0, 10)))
