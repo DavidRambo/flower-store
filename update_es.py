bdb107b8c45e76c66618c78096bd6f5148d6ea94
David Rambo <davidrambo@mailfence.com>
2023-06-26 15:02:15 -0700

    Add some test scripts for elasticsearch on searchbox

--- update_es.py
+++ update_es.py
@@ -8,17 +8,38 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-url_db: str = os.environ.get("SUPABASE_URL")
-key_db: str = os.environ.get("SUPABASE_KEY")
 
-supa: Client = create_client(url_db, key_db)
+def setup_supabase() -> Client:
+    url_db: str = os.environ.get("SUPABASE_URL")
+    key_db: str = os.environ.get("SUPABASE_KEY")
 
-url_es: str = urlparse(os.environ.get("ELASTICSEARCH_URL"))
-key_es: str = os.environ.get("ELASTICSEARCH_KEY")
-es = Elasticsearch(
-    "https://" + url_es.hostname + ":433",
-    basic_auth=(url_es.username, url_es.password),
-)
+    supa: Client = create_client(url_db, key_db)
+
+    return supa
+
+
+def setup_elastic() -> Elasticsearch:
+    url_es: str = urlparse(os.environ.get("ELASTICSEARCH_URL"))
+    key_es: str = os.environ.get("ELASTICSEARCH_KEY")
+    es = Elasticsearch(
+        "https://" + url_es.hostname + ":443",
+        basic_auth=(url_es.username, url_es.password),
+    )
+
+    return es
+
+
+# ignore 400 caused by IndexAlreadyExistsException
+# es.indices.create(index="flower", ignore=400)
+
+
+def create_test_entry(es: Elasticsearch) -> None:
+    # Create flower name entry.
+    doc = {
+        "name": "test",
+    }
+    res = es.index(index="flower", id=1, document=doc)
+    print(res["created"])
 
 
 # def pop_flowers():
